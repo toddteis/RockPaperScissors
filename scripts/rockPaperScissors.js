caf4bdf4f627ad7bea5499b40933bde741b52e0e
toddt <toddteis@gmail.com>
2021-09-13 05:52:18 +1000

    testing of computerPlay function

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -1,6 +1,30 @@
 // create function that randomly generates one of: rock, paper or scissors.
-// create variable to hold value.
+function computerPlay() {
+// create variable to hold return value.
+let computerChoice;
 // randomly generate a number between 1 and 3.
+let randNum = Math.round(Math.random()*2)+1;
+console.log(`randNum value: ${randNum}`);
+
 // if number is 1 then return rock.
 // if number is 2 then return paper.
 // else return scissors.
+if (randNum === 1) {
+    computerChoice = "rock";
+} else if (randNum === 2) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+return computerChoice;
+}
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);
+console.log(`computerPlay value: ${computerPlay()}`);

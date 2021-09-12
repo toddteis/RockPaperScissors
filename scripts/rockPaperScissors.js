5e38fa21acad1ef26b0375d2f3a26c0b0ed5742a
toddt <toddteis@gmail.com>
2021-09-13 05:30:13 +1000

    Pseudo code for randomly generating computer choice

---
+++ scripts/rockPaperScissors.js
@@ -0,0 +1,6 @@
+// create function that randomly generates one of: rock, paper or scissors.
+// create variable to hold value.
+// randomly generate a number between 1 and 3.
+// if number is 1 then return rock.
+// if number is 2 then return paper.
+// else return scissors.

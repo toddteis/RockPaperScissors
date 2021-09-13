2ba381f5a224280c5f67dd600bb58a5fe5ad2d63
toddt <toddteis@gmail.com>
2021-09-14 05:36:05 +1000

    Testing of playRound function

--- scripts/rockPaperScissors.js
+++ scripts/rockPaperScissors.js
@@ -1,22 +1,56 @@
 // Create a variable to store the players choice.
+const playerSelection = "scissors";
+
 // Create a variable to store the computers choice.
+const computerSelection = computerPlay();
+
 // Create a function that takes the players and computers choices as
 // parameters and plays a round of Rock, Paper, Scissors.
-    // Inside the function:
-        // Create a variable to hold the result.
-        // Convert player choice to all lower-capitals.
-        // If player choice is rock and computer choice is rock, its a  draw.
-        // else if player choice is rock and computer choice is paper, computer wins.
-        // else if player choice is rock and computer choice is scissors, player wins.
-        // else if player choice is paper and computer choice is rock, player wins.
-        // else if player choice is paper and computer choice is paper, its a draw.
-        // else if player choice is paper and computer choice is scissors, computer wins.
-        // else if player choice is scissors and computer choice is rock, computer wins.
-        // else if player choice is scissors and computer choice is paper, player wins.
-        // else player choice is scissors and computer choice is scissors, its a draw.
-        // return the result variable.
-// console log the function with player and computer selections.
+function playRound(playerSelection, computerSelection) {
+    // Create a variable to hold the result.
+    let result;
 
+    // Convert player choice to all lower-capitals.
+    let lowerCasePlayerSelection = playerSelection.toLowerCase();
+
+    // If player choice is rock and computer choice is rock, its a  draw.
+    if (lowerCasePlayerSelection === "rock" && computerSelection === "rock") {
+        result = "Rock against Rock equals a draw!!";
+        // else if player choice is rock and computer choice is paper, computer wins.
+    } else if (lowerCasePlayerSelection === "rock" && computerSelection === "paper") {
+        result = "You lose, Paper beats Rock!!";
+        // else if player choice is rock and computer choice is scissors, player wins.
+    } else if (lowerCasePlayerSelection === "rock" && computerSelection === "scissors") {
+        result = "You win, Rock beats Scissors!!";
+        // else if player choice is paper and computer choice is rock, player wins.
+    } else if (lowerCasePlayerSelection === "paper" && computerSelection === "rock") {
+        result = "You win, Paper beats Rock!!"
+        // else if player choice is paper and computer choice is paper, its a draw.
+    } else if (lowerCasePlayerSelection === "paper" && computerSelection === "paper") {
+        result = "Paper against Paper equals a draw!!";
+        // else if player choice is paper and computer choice is scissors, computer wins.
+    } else if (lowerCasePlayerSelection === "paper" && computerSelection === "scissors") {
+        result = "You lose, Scissors beats Paper!!";
+        // else if player choice is scissors and computer choice is rock, computer wins.
+    } else if (lowerCasePlayerSelection === "scissors" && computerSelection === "rock") {
+        result = "You lose, Rock beats Scissors!!";
+        // else if player choice is scissors and computer choice is paper, player wins.
+    } else if (lowerCasePlayerSelection === "scissors" && computerSelection === "rock") {
+        result = "You win, Scissors beats Rock!!";
+        // else if player choice is scissors and computer choice is paper, player wins.
+    } else if (lowerCasePlayerSelection === "scissors" && computerSelection === "paper") {
+        result = "You win, Scissors beats Paper!!";
+        // else player choice is scissors and computer choice is scissors, its a draw.
+    } else if (lowerCasePlayerSelection === "scissors" && computerSelection === "scissors") {
+        result = "Scissors against Scissors equals a draw!!";
+    } 
+
+    // return the result variable.
+    return result;
+}
+    
+// console log the function with player and computer selections.
+console.log(playRound(playerSelection, computerSelection));
 
 
 // create function that randomly generates one of: rock, paper or scissors.
